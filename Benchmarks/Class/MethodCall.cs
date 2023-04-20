abc49b15795204b4b1762909eb214e90b4aefea8
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2023-04-20 20:44:13 +0200

    Extra method call benchmarks

--- Benchmarks/Class/MethodCall.cs
+++ Benchmarks/Class/MethodCall.cs
@@ -1,5 +1,6 @@
 ﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Order;
+using System.Runtime.CompilerServices;
 
 namespace Benchmarks.Class
 {
@@ -8,6 +9,7 @@ namespace Benchmarks.Class
 	{
 		private interface IClass
 		{
+			[MethodImpl(MethodImplOptions.NoInlining)]
 			int InterfaceMethod();
 		}
 
@@ -15,23 +17,34 @@ namespace Benchmarks.Class
 		{
 			protected int m_Next = 0;
 
+			[MethodImpl(MethodImplOptions.NoInlining)]
 			public int Method()
 			{
 				return ++m_Next;
 			}
 
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			public int InlinedMethod()
+			{
+				return ++m_Next;
+			}
+
+			[MethodImpl(MethodImplOptions.NoInlining)]
 			public virtual int VirtualMethod()
 			{
 				return ++m_Next;
 			}
 
+			[MethodImpl(MethodImplOptions.NoInlining)]
 			public virtual int VirtualOverridenMethod()
 			{
 				return ++m_Next;
 			}
 
+			[MethodImpl(MethodImplOptions.NoInlining)]
 			public abstract int AbstractMethod();
 
+			[MethodImpl(MethodImplOptions.NoInlining)]
 			public int InterfaceMethod()
 			{
 				return ++m_Next;
@@ -40,12 +53,36 @@ namespace Benchmarks.Class
 
 		private class TestClass : ABaseClass
 		{
+			[MethodImpl(MethodImplOptions.NoInlining)]
 			public override int VirtualOverridenMethod()
 			{
 				return ++m_Next;
 			}
 
+			[MethodImpl(MethodImplOptions.NoInlining)]
 			public override int AbstractMethod()
+			{
+				return ++m_Next;
+			}
+		}
+
+		private struct TestStruct : IClass
+		{
+			private int m_Next;
+
+			public TestStruct()
+			{
+				m_Next = 0;
+			}
+
+			[MethodImpl(MethodImplOptions.NoInlining)]
+			public int InterfaceMethod()
+			{
+				return ++m_Next;
+			}
+
+			[MethodImpl(MethodImplOptions.NoInlining)]
+			public int Method()
 			{
 				return ++m_Next;
 			}
@@ -62,6 +99,42 @@ namespace Benchmarks.Class
 			for(int x = 0; x < iterationCount; ++x)
 			{
 				value += test.Method();
+			}
+		}
+
+		[Benchmark]
+		public void InlinedMethod()
+		{
+			int value = 0;
+			TestClass test = new TestClass();
+
+			for(int x = 0; x < iterationCount; ++x)
+			{
+				value += test.InlinedMethod();
+			}
+		}
+
+		[Benchmark]
+		public void StructMethod()
+		{
+			int value = 0;
+			TestStruct test = new TestStruct();
+
+			for(int x = 0; x < iterationCount; ++x)
+			{
+				value += test.Method();
+			}
+		}
+
+		[Benchmark]
+		public void StructViaInterfaceMethod()
+		{
+			int value = 0;
+			TestStruct test = new TestStruct();
+
+			for(int x = 0; x < iterationCount; ++x)
+			{
+				value += test.InterfaceMethod();
 			}
 		}
 
